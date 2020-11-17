7e327d2c4c3c216237c4d7f1839f61e65997d5b1
Dana Miller <dmgt@users.noreply.github.com>
2020-11-17 17:36:15 -0500

    Add context to shell session

--- microblog.py
+++ microblog.py
@@ -1,3 +1,8 @@
 # Main application module
 # Imports app variable (Flask application instance) that is member of app package
-from app import app
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor #  adds database instance and models to shell session
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
