e366593026914e615cfebda0d63885c4affac1da
Dana Miller <dmgt@users.noreply.github.com>
2020-11-17 15:41:58 -0500

    Add mock user

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,13 @@ from app import app
 @app.route('/')  # Decorators create association between  '/', '/index' and function below
 @app.route('/index')
 def index():
-    return "Hello World"
+    user = {'username': 'Dana'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
