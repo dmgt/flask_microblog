298180984cc17507a15959e132684016b8ae2473
Dana Miller <dmgt@users.noreply.github.com>
2020-11-17 17:27:28 -0500

    Updated db with "posts" table

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 # User database model 
-
+from datetime import datetime
 from app import db
 
 class User(db.Model): # User class inherits from db.Model
@@ -7,6 +7,16 @@ class User(db.Model): # User class inherits from db.Model
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic') # Defiens 1:many relationship
 
     def __repr__(self):  # "tells Python how to print objects of this class"
         return '<User {}>'.format(self.username)    
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
