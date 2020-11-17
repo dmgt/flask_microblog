3eb9fa77f77a48c1a95c1582073243b5cba30a34
Dana Miller <dmgt@users.noreply.github.com>
2020-11-17 17:04:19 -0500

    Add flask sql alchemy and migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from config import Config  # From config.py
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)  # Create application object
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes  # Imported at end to avoid circular imports
+from app import routes, models  # Imported at end to avoid circular imports
+                                # models defines structre of db
--- config.py
+++ config.py
@@ -3,3 +3,6 @@ import os
 #  configuration settings defined as class variables inside the Config class
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
