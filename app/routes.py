29f9affa42c97958d6d3cfb66f9483e6fb4d856a
Dana Miller <dmgt@users.noreply.github.com>
2020-11-17 15:47:37 -0500

    Add template, rewrite view function accordingly

--- app/routes.py
+++ app/routes.py
@@ -5,12 +5,4 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Dana'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    return render_template('index.html', title='Home', user = user)
