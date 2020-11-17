fdf54d162e494f45e7a710e25bd367919ed2dcf1
Dana Miller <dmgt@users.noreply.github.com>
2020-11-17 17:55:13 -0500

    Add basic bootstrap CSS

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from config import Config  # From config.py
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)  # Create application object
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+bootstrap = Bootstrap(app)
 
 from app import routes, models  # Imported at end to avoid circular imports
                                 # models defines structre of db
